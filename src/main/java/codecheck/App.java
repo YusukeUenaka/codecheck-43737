0efcf3221a40bbc6de7c7541d4ecb8ef46a0cf49
uenaka.yusuke <uenaka.yusuke@tis.co.jp>
2018-06-20 22:43:53 +0900

    完成

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,8 +1,6 @@
 package codecheck;
 
 public class App {
-
-
     public static void main(String[] args) {
         String[] base = new String[2];
         String[] str = new String[1000];
@@ -11,31 +9,23 @@ public class App {
         int input = 0;
         int count = 0;
         StringBuilder output = new StringBuilder();
-        //String output = null;
         //String output = String.format("argv[%s]: %s", i, args[i]);
         for (int i = 0, l = args.length; i < l; i++) {
             if (!args[i].toString().contains(":")) {
                 input = Integer.parseInt(args[i].toString());
-                System.out.println(input);
             } else {
                 base = args[i].split(":", 0);
                 modNum[i] = base[0].toString();
                 str[i] = base[1].toString();
-                System.out.println(modNum[i].toString());
-                System.out.println(str[i].toString());
             }
             count = i;
         }
         for (int i = 0, l = modNum.length; i < count; i++) {
-            System.out.println(i);
             int modnum = Integer.parseInt(modNum[i].toString());
-            System.out.println(modnum);
             if (input%modnum  == 0) {
                 output = output.append(str[i].toString());
             }
-            System.out.println(output);
         }
         System.out.println(output);
     }
-
 }
