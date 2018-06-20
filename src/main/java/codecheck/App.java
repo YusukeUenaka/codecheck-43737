05c6d63df090c9b81607a4672469caaf68fd762b
uenaka.yusuke <uenaka.yusuke@tis.co.jp>
2018-06-20 22:22:19 +0900

    dfas

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,11 +2,10 @@ package codecheck;
 
 public class App {
 
-
     public static void main(String[] args) {
-        String[] base = null;
+        String[] base = new String[2];
         String[] str = null;
-        int modNum = 0;
+        String[] modNum = null;
         String text = null;
         int input = 0;
         String output = null;
@@ -17,19 +16,18 @@ public class App {
                 System.out.println(input);
             } else {
                 base = args[i].split(":", 0);
-                if (!base[i+1].isEmpty()) {
-                    str[i] = base[i + 1];
-                }
+                modNum[i] = base[0].toString();
+                str[i] = base[1].toString();
+                System.out.println(modNum[i].toString());
+                System.out.println(str[i].toString());
             }
         }
-        for (int i = 0, l = base.length; i < l-1; i++) {
-            modNum = Integer.parseInt(base[i].toString());
-            text = str[i].toString();
-            System.out.println(modNum);
-            System.out.println(text);
-            if (input%modNum == 0) {
-                output = output.concat(text);
+        for (int i = 0, l = modNum.length; i < l; i++) {
+            int modnum = Integer.parseInt(modNum[i].toString());
+            if (input%modnum  == 0) {
+                output = output.concat(str[i].toString());
             }
+            System.out.println(modnum);
         }
         System.out.println(output);
     }
